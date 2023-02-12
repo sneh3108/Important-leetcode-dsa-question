04b435d926636d80dd6235aa914b88a6e6f10e22
SNEH RANJAN <100509471+sneh3108@users.noreply.github.com>
2023-02-13 00:08:09 +0530

    Create 2562. Find the Array Concatenation Value.cpp

---
+++ 2562. Find the Array Concatenation Value.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long findTheArrayConcVal(vector<int>& nums) {
+        long long res=0;
+        int n=nums.size();
+        for(int i=0;i<n/2;i++){
+          // to_string() function use for changing integer to string
+          string temp=to_string(nums[i])+to_string(nums[n-1-i]); 
+          // stol() function use to change string to integer value
+          res+=stol(temp);
+        }
+      if(n%2==1) res+=nums[n/2];
+      return res;
+
+    }
+};
